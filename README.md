9b2701bdb032e8dcf3ea255ef4e08cde9d34743e
Sahil Rajput <sahilrajput03@gmail.com>
2023-10-15 01:10:01 +0530

    Init.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Learn Web Components
+
+Library Used - Lit
+
+- MDN Docs - Web Components: [Click here](https://developer.mozilla.org/en-US/docs/Web/API/Web_components)
+- MDN Examples: [Click here](https://github.com/mdn/web-components-examples)
+- Lit Library for Web Components: [Click here](https://lit.dev/docs/getting-started/)
+- Download frontendmasters.com Courses: [Click here](https://frontendmasters.com/courses/web-components/introduction/)
---
+++ simple-greeting.js
@@ -0,0 +1,21 @@
+import {LitElement, css,html} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js'
+// Below line not working (TESTED)
+// import {html, css, LitElement} from 'lit';
+
+export class SimpleGreeting extends LitElement {
+  static styles = css`p { color: blue }`;
+
+  static properties = {
+    name: {type: String},
+  };
+
+  constructor() {
+    super();
+    this.name = 'Somebody';
+  }
+
+  render() {
+    return html`<p>Hello, ${this.name}!</p>`;
+  }
+}
+customElements.define('simple-greeting', SimpleGreeting);
