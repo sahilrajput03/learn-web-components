06e9e73996aacf5327cce5ef6e0dc34a7967aea1
Sahil Rajput <sahilrajput03@gmail.com>
2025-09-22 22:02:54 +0530

    Update.

--- README.md
+++ README.md
@@ -4,5 +4,13 @@ Library Used - Lit
 
 - MDN Docs - Web Components: [Click here](https://developer.mozilla.org/en-US/docs/Web/API/Web_components)
 - MDN Examples: [Click here](https://github.com/mdn/web-components-examples)
-- Lit Library for Web Components: [Click here](https://lit.dev/docs/getting-started/)
+- ❤️ Lit Library for Web Components from Google: [Click here](https://lit.dev/docs/getting-started/)
 - Download frontendmasters.com Courses: [Click here](https://frontendmasters.com/courses/web-components/introduction/)
+
+## Q. Can a compoent name be one word like <user> ?
+
+No, a custom element name (web component) cannot be a single word like <user>. According to the HTML specification, custom element names must contain a hyphen (e.g., <my-user>, <user-profile>). This prevents conflicts with existing and future standard HTML elements.
+
+# Q. Can `<my-counter></my-counter>` be written as `<my-counter />` ?
+
+No.
--- components.js
+++ components.js
@@ -1,57 +1,107 @@
 // @ts-nocheck
 import { LitElement, css, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
 
-export class SimpleGreeting extends LitElement {
-  static get styles() {
-    return css`p { color: var(--greeting-color, blue); }`;
+export class SimpleGreeting extends LitElement { // & Component
+  constructor() {
+    super();
+    // ❤️Learn: `this.name` is undefined here even when passed to
+    //    component because properties are not yet initialized thus they
+    //    are `undefined` here.
+    // console.log('this.name?', this.name) // undefined
+
+    // ❤️Learn: Setting default value of property (s) go here:
+    this.name = 'somebody';
+
+    // Learn: `this` Prints the instance of the component being created
+    // console.log('`this` in SimpleGreeting component?', this); 
   }
 
-  updated(changedProperties) {
-    if (changedProperties.has('color')) {
-      this.style.setProperty('--greeting-color', this.color || 'blue');
-    }
-  }
-
+  // ❤️Learn: Define reactive property (s) for your LitElement
+  //      component. Updating `this.name` and `this.color` will trigger
+  //      rerender.
   static properties = {
     name: { type: String },
     color: { type: String },
   };
 
-  constructor() {
-    super();
-    this.name = 'somebody';
-  }
-
   render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`<p style="color: ${this.color}">Hello, ${this.name}!</p>`;
   }
 }
 customElements.define('simple-greeting', SimpleGreeting);
 
-
-export class Counter extends LitElement {
-  static get styles() {
-    return css``;
-  }
-
-  updated(changedProperties) {
-    if (changedProperties.has('count')) {
-      this.count++;
-    }
-  }
-
-  static properties = {
-    name: { type: String },
-    color: { type: String },
-  };
-
+export class Counter extends LitElement { // & Component
   constructor() {
     super();
-    this.count = 0;
+    this.count = 0; // set initial value of `count`
   }
 
+  static properties = { count: { type: Number }, };
+
+  increment() { this.count++; }
+
+  updated(changedProperties) {
+    console.log('updated - changedProperties.get(`count`)?', changedProperties.get('count')); // Prints previous count value
+    console.log('updated - this.count', this.count); // Prints updated count value
+  }
+
+  static get styles() { return css`button { color: pink; background: black; }`; }
+
   render() {
-    return html`<button>${count}</button>`;
+    return html`<button @click="${this.increment}">${this.count}</button>`;
   }
 }
-customElements.define('counter', Counter);
+customElements.define('my-counter', Counter);
+
+export class UserProfile extends LitElement {
+  constructor() {
+    super();
+  }
+  static properties = {
+    name: { type: String },
+    age: { type: String },
+    gender: { type: String },
+  };
+  // Lifecycle method (of LitElement)
+  firstUpdated(changedProperties) {
+    // console.log('firstUpdated?', changedProperties);
+    // ❤️Learn: Errors handling goes here for properties
+    const errors = [];
+    if (!this.name) { errors.push('Please provide name property.'); }
+    if (!this.age) { errors.push('Please provide age property.'); }
+    if (!this.gender) { errors.push('Please provide gender property.'); }
+    if (errors.length) { throw ['Missing properties:', ...errors].join('\n'); }
+  }
+
+  // This lifecycle method (of LitElement) is run after `firstUpdated`
+  updated(changedProperties) {
+    console.log('updated - changedProperties?', changedProperties); // { 'age': previousValueHere }
+    console.log('updated - changedProperties.get(`age`) ?', changedProperties.get('age')); // Prints previous age value
+    console.log('this.age?', this.age); // Prints updated age value
+  }
+
+  static get styles() {
+    return css`
+    .container {
+      border: 2px solid black;
+      width: fit-content;
+      padding: 10px; 
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+    }
+    button { color: purple; background: white; border-color: purple; }`;
+  }
+
+  increment() { this.age++; }
+
+  render() {
+    return html`
+   <div class="container"> 
+    <div>Name: ${this.name}</div>
+    <button @click="${this.increment}">Age: ${this.age}</button>
+    <div>Gender: ${this.gender}</div>
+    `;
+  }
+}
+customElements.define('user-profile', UserProfile);
